e72280499bba86bd46dd46d824cd75315a4ccefc
Aleksey <59054924+SobolAlexey@users.noreply.github.com>
2022-01-18 05:01:15 +0300

    created StartView.tsx [0.4]

---
+++ app/components/screens/StartView.tsx
@@ -0,0 +1,9 @@
+import React, { FC, useState } from "react";
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+const StartView: FC = () => {
+  return <View></View>;
+};
+
+export default StartView;
