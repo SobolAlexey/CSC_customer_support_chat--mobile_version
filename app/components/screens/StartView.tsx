38796f952a84b3137301442769bb6269f6193947
Aleksey <59054924+SobolAlexey@users.noreply.github.com>
2022-01-18 05:09:23 +0300

    add field on StartView.tsx and style from StartView.tsx [1.2]

--- app/components/screens/StartView.tsx
+++ app/components/screens/StartView.tsx
@@ -1,9 +1,35 @@
 import React, { FC, useState } from "react";
 import { View } from "react-native";
 import styled from "styled-components/native";
+import Field from "../ui/Field";
+
+const ViewMain = styled.View`
+  max-height: 100%;
+  height: 100%;
+  color: rgb(73, 73, 73);
+  margin-left: 20px;
+  margin-right: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-bottom: 90px;
+  padding-top: 190px;
+  font-weight: 400;
+  font-size: 1.2rem;
+  color: #52595e;
+`;
 
 const StartView: FC = () => {
-  return <View></View>;
+  const [name, setName] = useState<string>("");
+  return (
+    <ViewMain>
+      <Field
+        val={name}
+        placeholder='Enter Name'
+        onChange={(val) => setName(val)}
+      />
+    </ViewMain>
+  );
 };
 
 export default StartView;
